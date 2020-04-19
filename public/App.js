c51c58ceb35fdb872c8c8aaf6bd7377136019684
Elom Atsou Agboka <opentech.consult@gmail.com>
2020-04-19 23:57:29 +0000

    Separate Script File Move JXS transformation from runtime stage to build stage
    - Create a JSX file called App.jsx and put it in public folder
    - replace the inline script within index.html with a reference to an external resource
    - create a new directory called src in the root directory of the project
    - move App.js from /public into /src
    - install babel core and babel cli
    - install jsx transform preset "preset-react"
    - run the command to transform JSX to JS using babel/react preset
    - change reference in index.html and remove "text/babel" type specification
    - get rid of babel/core script library specification

---
+++ public/App.js
@@ -0,0 +1,4 @@
+const element = /*#__PURE__*/React.createElement("div", {
+  title: "Outer div"
+}, /*#__PURE__*/React.createElement("h1", null, "Hello World!"));
+ReactDOM.render(element, document.getElementById('contents'));
