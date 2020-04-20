ae87516636ba57c2835af839f4b11ddced1294b5
Elom Atsou Agboka <opentech.consult@gmail.com>
2020-04-20 01:25:03 +0000

    Older Browser Support make changes to support some advanced ES2015 make changes to support all the older features in older browsers

--- public/App.js
+++ public/App.js
@@ -1,4 +1,11 @@
-const element = /*#__PURE__*/React.createElement("div", {
+"use strict";
+
+var continents = ['Africa', 'America', 'Asia', 'Australia', 'Europa'];
+var helloContinents = Array.from(continents, function (c) {
+  return "Hello ".concat(c, "!");
+});
+var message = helloContinents.join( /*#__PURE__*/React.createElement("br", null));
+var element = /*#__PURE__*/React.createElement("div", {
   title: "Outer div"
-}, /*#__PURE__*/React.createElement("h1", null, "Hello World!"));
+}, /*#__PURE__*/React.createElement("h1", null, message));
 ReactDOM.render(element, document.getElementById('contents'));
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europa'];
+const helloContinents = Array.from(continents, c => `Hello ${c}!`);
+const message = helloContinents.join(' ');
 const element = (
     <div title="Outer div">
-        <h1>Hello World!</h1>
+        <h1>{message}</h1>
     </div>
 );
 ReactDOM.render(element, document.getElementById('contents'));
