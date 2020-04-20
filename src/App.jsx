ca2ec487fe218f3d4b50956caf6a9a128d39433c
Elom Atsou Agboka <opentech.consult@gmail.com>
2020-04-20 12:15:13 +0000

    React class Objective: convert simple single-line JSX into simple React Component -Change the hello world example from simple element to use a React class called HelloWorld extended from React.Component -within the class define the render method which should return an element -use the same JSX div inside the `return()` section -move all code for message construction to within the render function so it remains encapsulated within the scope where it is needed -instantiate the HelloWorld component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europa'];
-const helloContinents = Array.from(continents, c => `Hello ${c}!`);
-const message = helloContinents.join(' ');
-const element = (
-    <div title="Outer div">
-        <h1>{message}</h1>
-    </div>
-);
+class HelloWorld extends React.Component {
+
+    render() {
+        const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europa'];
+        const helloContinents = Array.from(continents, c => `Hello ${c}!`);
+        const message = helloContinents.join(' ');
+        
+        return (
+            <div title="Outer div">
+                <h1>{message}</h1>
+            </div>
+        );
+    }
+}
+const element = <HelloWorld/>;
 ReactDOM.render(element, document.getElementById('contents'));
