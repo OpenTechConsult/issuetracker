b8557903e62f71f45e02f1f85bd6006ac76807cd
Elom Atsou Agboka <opentech.consult@gmail.com>
2020-04-20 01:57:31 +0000

    Automate the server start and client jsx compilation process
    add a script called compile that use babel to do all the jsx transform
    add script watch that add detect changes in the src folder an compile to .js in public
    install nodemon
    change the start command to watch for changes in the server folder and compile and launc the server

--- public/App.js
+++ public/App.js
@@ -4,7 +4,7 @@ var continents = ['Africa', 'America', 'Asia', 'Australia', 'Europa'];
 var helloContinents = Array.from(continents, function (c) {
   return "Hello ".concat(c, "!");
 });
-var message = helloContinents.join( /*#__PURE__*/React.createElement("br", null));
+var message = helloContinents.join(' ');
 var element = /*#__PURE__*/React.createElement("div", {
   title: "Outer div"
 }, /*#__PURE__*/React.createElement("h1", null, message));
