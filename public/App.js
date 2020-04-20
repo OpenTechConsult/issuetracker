001ed2c54397b24b46350167fd9b5e61cff4166b
Elom Atsou Agboka <opentech.consult@gmail.com>
2020-04-20 18:13:48 +0000

    Passing Data using Children explore another way to pass data to component by using content of the HTML-like nodeof the component. Here we change the <IssueRow/> to <IssueRow></IssueRow> and passing that data by puting inside the begin and closing tags. To access the data in the component class we use the this.props.children

--- public/App.js
+++ public/App.js
@@ -93,13 +93,11 @@ var IssueTable = /*#__PURE__*/function (_React$Component3) {
         style: rowStyle
       }, "Title"))), /*#__PURE__*/React.createElement("tbody", null, /*#__PURE__*/React.createElement(IssueRow, {
         rowStyle: rowStyle,
-        issue_id: 1,
-        issue_title: "Error in console when clicking Add"
-      }), /*#__PURE__*/React.createElement(IssueRow, {
+        issue_id: 1
+      }, "Error in console when clicking Add"), /*#__PURE__*/React.createElement(IssueRow, {
         rowStyle: rowStyle,
-        issue_id: 2,
-        issue_title: "Missing bottom border on panel"
-      })));
+        issue_id: 2
+      }, /*#__PURE__*/React.createElement("div", null, "Missing ", /*#__PURE__*/React.createElement("b", null, "bottom"), " border on panel"))));
     }
   }]);
 
@@ -146,7 +144,7 @@ var IssueRow = /*#__PURE__*/function (_React$Component5) {
         style: style
       }, this.props.issue_id), /*#__PURE__*/React.createElement("td", {
         style: style
-      }, this.props.issue_title));
+      }, this.props.children));
     }
   }]);
 
