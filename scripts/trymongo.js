3bc73066d48fa9bf5dbff5608adad156155051c7
Elom Atsou Agboka <opentech.consult@gmail.com>
2020-04-30 16:09:08 +0000

    MongoDB NodeJS Driver
    Install the MongoDB NodeJS driver
    create a file 'trymongo.js' inside a newly created directory called 'script'
    Play with the mongodb nodejs driver and experiment with it

---
+++ scripts/trymongo.js
@@ -0,0 +1,70 @@
+const { MongoClient } = require('mongodb');
+
+const url = 'mongodb://localhost/issuetracker';
+
+testWithAsync();
+
+// testWithCallbacks(function (err) {
+//     if (err) {
+//         console.log(err);
+//     }
+// });
+
+function testWithCallbacks(callback) {
+    
+    console.log('\n -- testWithCallbacks')
+    
+    const client = new MongoClient(url, { useNewUrlParser: true });
+    client.connect(function (err, client) {
+        if (err) {
+            callback(err);
+            return;
+        }
+        console.log('Connected to MongoDB');
+
+        const db = client.db();
+        const collection = db.collection('employees');
+        
+        const employee = { id: 5, name: 'A. Callback', age: 23 };
+        collection.insertOne(employee, function (err, result) {
+            if (err) {
+                client.close();
+                callback(err);
+                return;
+            }
+            console.log('Result of insert:\n', result.insertedId);
+            collection.find({ _id: result.insertedId }).toArray(function (err, docs) {
+                if (err) {
+                    client.close();
+                    callback(err);
+                    return;
+                }
+                console.log('Result of find:\n', docs);
+                client.close();
+                callback(err);
+            });
+        });
+    });
+}
+
+async function testWithAsync() {
+    console.log('\n -- Test with Async');
+    const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true});
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB');
+        const db = client.db();
+        const collection = db.collection('employees');
+
+        const employee = { id: 6, name: 'B. Async', age: 16};
+        const result = await collection.insertOne(employee);
+        console.log('Result of insert:\n', result.insertedId);
+        const docs = await collection.find({ _id: result.insertedId}).toArray();
+        console.log('Result of find:\n', docs);
+    } catch (error) {
+        console.log(error);
+    } finally {
+        client.close();
+    }
+    
+}
